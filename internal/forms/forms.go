52275f561324d8d4c1460b6fcafe8d46c85b6276
Doha saleh <doha.m.isaleh@outlook.com>
2026-09-20 19:35:26 +0300

    Added form validation

--- internal/forms/forms.go
+++ internal/forms/forms.go
@@ -1,8 +1,12 @@
 package forms
 
 import (
+	"fmt"
 	"net/http"
 	"net/url"
+	"strings"
+
+	"github.com/asaskevich/govalidator/v12"
 )
 
 type Form struct {
@@ -22,13 +26,36 @@ func New(data url.Values) *Form {
 func (f *Form) Has(field string, r *http.Request) bool {
 	x := r.Form.Get(field)
 	if x == "" {
-		f.Errors.Add(field, "This field cannot be blank")
 
 		return false
 	}
 	return true
 }
 
+func (f *Form) Required(fields ...string) {
+	for _, field := range fields {
+		value := f.Get(field)
+		if strings.TrimSpace(value) == "" {
+			f.Errors.Add(field, "this field cant be blank")
+		}
+	}
+
+}
 func (f *Form) Valid() bool {
 	return len(f.Errors) == 0
+}
+
+func (f *Form) MinLength(field string, length int, r *http.Request) bool {
+	x := r.Form.Get(field)
+	if len(x) < length {
+		f.Errors.Add(field, fmt.Sprintf("this field must be at least %d characters long", length))
+		return false
+	}
+	return true
+}
+
+func (f *Form) IsEmail(field string) {
+	if !govalidator.IsEmail(f.Get(field)) {
+		f.Errors.Add(field, "Invalid email address")
+	}
 }
